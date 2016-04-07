94b16f3b059ef41c3be7ceb5387e837659e49b3d
ChristinaGaitan <christina.gaitan@gmail.com>
2016-04-07 10:23:51 -0500

    Example with basic operations

---
+++ basicOperations.go
@@ -0,0 +1,21 @@
+// Valores
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// Concatenacion
+	fmt.Println("go" + "lang")
+	fmt.Println("5 + 5 =", 5+5)
+
+	// Manejo de flotantes
+	fmt.Println("7.5/3.2 = ", 7.5/3.2)
+
+	// Manejo de booleanos
+	fmt.Println("Booleanos = ")
+	fmt.Println("true AND false = ", true && false)
+	fmt.Println("true OR false = ", true || false)
+	fmt.Println("NOT false = ", !false)
+}
