78fe8ff8ca10d288188c5dfd936b8b79ca566065
ChristinaGaitan <christina.gaitan@gmail.com>
2016-04-11 09:56:38 -0500

    Ejemplo con slices

---
+++ slices.go
@@ -0,0 +1,30 @@
+// Tipos compuestos: Slices
+
+package main
+
+import "fmt"
+
+func main() {
+	// Declaración de slice
+	var numeros []int
+
+	for i:= 0; i < 10; i++ {
+		numeros = append(numeros, i * 10)
+	}
+
+	for _, numero := range numeros {
+		fmt.Println(numero)
+	}
+
+	frutas := []string{"manzana", "naranja", "pera", "sandía", "aguacate"}
+	for i, fruta := range frutas {
+		fmt.Printf("Index: %d - Fruta: %s\n", i, fruta)
+	}
+
+	// Tomar un slice de índice 1 y 2
+	slice := frutas [1:3]
+
+	for i, fruta := range slice {
+		fmt.Printf("Index: %d - Fruta: %s\n", i, fruta)
+	}
+}
